f8f3811e837798ce66858483606607d9f78c7e76
PreetiMehta111 <preetikumarimehta267@gmail.com>
2025-11-19 21:49:53 +0545

    Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter marks: ");
+        int.TryParse(Console.ReadLine(), out int marks);
+
+        Console.Write("Enter total: ");
+        int.TryParse(Console.ReadLine(), out int total);
+
+        // Breakpoint 1
+
+        double percentage = (double)marks / total * 100;
+
+        // Breakpoint 2
+
+        Console.WriteLine("Percentage: " + percentage);
+    }
+}
