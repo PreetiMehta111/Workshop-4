18a8fc3384b24a487a22e2f92baa7aa9166ac0f1
PreetiMehta111 <preetikumarimehta267@gmail.com>
2025-11-19 11:53:07 +0545

    Task 3

---
+++ Task 3/ParameterDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class ParameterDemo
+{
+    // 1. Method using ref keyword
+    public void Increase(ref int number)
+    {
+        number += 10;
+    }
+
+    // 2. Method using out keyword
+    public void GetFullName(out string fullname)
+    {
+        fullname = "Preeti Mehta";  
+    }
+
+    // 3. Method using params array
+    public int SumAll(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Task_3
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        ParameterDemo demo = new ParameterDemo();
+
+        //  Calling Increase using ref  
+        int value = 20;
+        demo.Increase(ref value);
+        Console.WriteLine("Value after Increase: " + value);
+
+        //  Calling GetFullName using out 
+        demo.GetFullName(out string fullName);
+        Console.WriteLine("Full Name: " + fullName);
+
+        //  Calling SumAll using params 
+        int total = demo.SumAll(5, 10, 15, 20);
+        Console.WriteLine("Sum of all numbers: " + total);
     }
 }
